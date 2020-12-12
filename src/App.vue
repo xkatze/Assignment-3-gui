d999cd79e2343e4684da2a445d978db6cf3ce366
xKatze <hejhjepower@gmail.com>
2020-12-12 17:55:59 +0100

    name fix

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <div id="nav">
       <router-link to="/">Home </router-link>
       <router-link to="/about">| About </router-link>
-      <router-link to="/hello">| Hello World </router-link>
+      <router-link to="/showJobs">| Hello World </router-link>
       <router-link to="/login" v-if="canSee()">| Login </router-link>
       <router-link to="/createManager" v-if="managerOnly()">| Create new Manager </router-link> 
       <router-link to="/makeExpense" v-if="modelOnly()">| Make Expense </router-link>
